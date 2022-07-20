037c9c9172bd6baabf4cbfd5db8835702f876000
mzimmer <mzimmer@zoios.net>
2022-07-20 23:23:28 +0200

    removed some debug telegram-log

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 curl -X POST \
      -H 'Content-Type: application/json' \
      -d @<(cat <<EOF
-{"chat_id": "$TelegramChatId", "text": "$(hostname) (BASH): Starting installation scirpt..."}
+{"chat_id": "$TelegramChatId", "text": "$(hostname): Starting installation scirpt..."}
 EOF
 ) https://api.telegram.org/bot$TelegramBotToken/sendMessage
 
@@ -9,30 +9,16 @@ apt purge -y -qq ansible
 apt install -y -qq python3 python3-pip
 python3 -m pip install ansible
 
-curl -X POST \
-     -H 'Content-Type: application/json' \
-     -d @<(cat <<EOF
-{"chat_id": "$TelegramChatId", "text": "$(hostname) (BASH): Updating installation scirpt..."}
-EOF
-) https://api.telegram.org/bot$TelegramBotToken/sendMessage
-
 git checkout -- .
 git pull
 rm -rf external_roles
 ansible-galaxy install -r requirements.yml
-
-curl -X POST \
-     -H 'Content-Type: application/json' \
-     -d @<(cat <<EOF
-{"chat_id": "$TelegramChatId", "text": "$(hostname) (BASH): Start Ansible to install display-software..."}
-EOF
-) https://api.telegram.org/bot$TelegramBotToken/sendMessage
 
 ansible-playbook site.yml -e api_key=$ApiKey -e monitor_user_key=$MonitorUserKey -e telegram_bot_token=$TelegramBotToken -e telegram_chat_id=$TelegramChatId
 
 curl -X POST \
      -H 'Content-Type: application/json' \
      -d @<(cat <<EOF
-{"chat_id": "$TelegramChatId", "text": "$(hostname) (BASH): Finished!"}
+{"chat_id": "$TelegramChatId", "text": "$(hostname): Installation script finished!"}
 EOF
 ) https://api.telegram.org/bot$TelegramBotToken/sendMessage
